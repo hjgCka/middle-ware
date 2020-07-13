310a0be57f86de45e7fee5e5ec8042ea5ed11c79
hjg <computer_cka@163.com>
2020-07-13 18:00:00 +0800

    更新资源地址。

--- mongo-driver-4/src/main/java/com/hjg/mongo/driver/MongoDriverApp.java
+++ mongo-driver-4/src/main/java/com/hjg/mongo/driver/MongoDriverApp.java
@@ -4,8 +4,8 @@ public class MongoDriverApp {
 
 
     public static void main(String[] args) {
-        String[] hosts = {"10.153.61.38"};
-        int[] ports = {8717};
+        String[] hosts = {"10.168.55.88"};
+        int[] ports = {27017};
         String database = "npdb", collectionName = "article";
         //MongoUtil1.printBooks(hosts, ports, database, collectionName);
 
