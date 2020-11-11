3683934fc6fd4350e16cc9aec8b14e38010c942e
hjg <computer_cka@163.com>
2020-11-12 00:34:25 +0800

    添加lookup的聚合方法。

--- mongo-driver-3/src/main/java/com/hjg/middle/ware/mongo/MongoUtil1.java
+++ mongo-driver-3/src/main/java/com/hjg/middle/ware/mongo/MongoUtil1.java
@@ -1,12 +1,17 @@
 package com.hjg.middle.ware.mongo;
 
 import com.mongodb.*;
+import com.mongodb.client.AggregateIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Variable;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -36,6 +41,36 @@ public class MongoUtil1 {
         MongoClient mongoClient = new MongoClient(serverAddresses, mongoCredential, mongoClientOptions);
 
         return mongoClient;
+    }
+
+    public AggregateIterable<Document> nativeLookUp(MongoDatabase database, String bossFileName, String taskContentFileName) {
+
+        String bossUserCollection = "tmp_boss_subscribe_user";
+        String taskContentCollection = "load_task_content";
+
+        List<Variable<String>> variables = Arrays.asList(new Variable<>("task_phone", "$phone"));
+
+        List<Bson> pipeline = Arrays.asList(
+                Aggregates.match(
+                        Filters.expr(
+                                new Document("$and", Arrays.asList(
+                                        new Document("$eq", Arrays.asList("$phone", "$$task_phone")),
+                                        new Document("$eq", Arrays.asList("$file_name", bossFileName)),
+                                        new Document("$ne", Arrays.asList("$differ_type", 3))
+                                ))
+                        )
+                )
+        );
+
+        MongoCollection<Document> taskCollection = database.getCollection(taskContentCollection);
+        AggregateIterable<Document> aggregateResult = taskCollection.aggregate(
+                Arrays.asList(
+                        Aggregates.match(Filters.and(Filters.eq("file_name", taskContentFileName), Filters.ne("can_use", false))),
+                        Aggregates.lookup(bossUserCollection, variables, pipeline, "boss_users")
+                )
+        ).allowDiskUse(true);
+
+        return aggregateResult;
     }
 
     public static void printBooks(String[] hosts, int[] ports, String databaseName, String collectionName) {
